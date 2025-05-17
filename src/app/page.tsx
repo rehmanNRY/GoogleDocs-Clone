1c0075cdba9af8d83fab71ee310911a561684088
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2025-05-17 07:00:00 +0500

    tiptap extensions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-amber-100">
       <p>Hello world</p>
-      <Button  >CLick me</Button>
+      {/* <Button  >CLick me</Button> */}
+      <Link href={"documents/123"} >Document id page</Link>
     </div>
   );
 }
