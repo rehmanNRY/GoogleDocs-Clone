bbce9ae5d1acd59ef237ec07bc3ba970ae55b299
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2025-05-18 07:00:00 +0500

    editor store & toolbar undo

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Editor } from './editor';
+import Toolbar from './toolbar';
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string; }>;
@@ -10,6 +11,7 @@ const page = async ({params}: DocumentIdPageProps) => {
 
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
+      <Toolbar />
       <Editor />
     </div>
   )
